e6b964ce2d7f8501b4f4a30229cd2025f47204b8
davidlawrencer <davidlrifkin+github@gmail.com>
2020-01-14 10:11:28 -0500

    Initial commit, adds view to window

---
+++ lifecycle-events/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  lifecycle-events
+//
+//  Created by David Rifkin on 1/14/20.
+//  Copyright © 2020 David Rifkin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
